683be8e85a1406909c264a6c70448a4ec7414d9e
Rajin Sakha <rajinsakha07@gmail.com>
2025-05-25 19:43:12 +0545

    added-vercel-speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import Navbar from "@/components/sections/navbar";
 import Footer from "@/components/sections/footer";
@@ -103,6 +104,7 @@ export default function RootLayout({
           </div>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
